bd5f4c960b8891156408826716708ede45a6eaad
Naoya Inada <naoina@kuune.org>
2020-07-14 21:51:44 +0900

    Fix for building and deployment on GitHub Actions

--- build.sh
+++ build.sh
@@ -9,8 +9,4 @@ EOF
     exit $1
 }
 
-go get -u -v -tags extended github.com/gohugoio/hugo
-go get -u -v github.com/tdewolff/minify/cmd/minify
-hugo
-publishdir=$(hugo config | awk '/^publishdir/ {gsub("\"", ""); print $3}')
-minify -r -o $publishdir $publishdir
+hugo --minify
