da07c1708caa2660d4c2689712bd735ac70bef12
Naoya Inada <naoina@kuune.org>
2021-06-01 22:12:46 +0900

    Improve convert.sh

--- convert.sh
+++ convert.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 usage() {
-    cat << EOF
-Usage: `basename $0`
+    cat <<EOF
+Usage: $(basename $0)
 EOF
     exit $1
 }
@@ -10,6 +10,11 @@ EOF
 #size=1200x630
 size=$2
 
-convert -define jpeg:size=$size -unsharp 12x6+0.5+0 -quality 92 -resize $size $1 $1
+convert \
+    -define jpeg:size=$size \
+    -quality 92 \
+    -resize $size \
+    $1 $1
+# -unsharp 12x6+0.5+0 \
 exiftool -all= $1
-mv $1 $(sha1sum $1 | cut -d ' ' -f 1).jpg
+mv $1 $(dirname $1)/$(sha1sum $1 | cut -d ' ' -f 1).jpg
