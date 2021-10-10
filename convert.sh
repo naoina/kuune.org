04f39bf8ca99ab692124121125eabfa39726928e
Naoya Inada <naoina@kuune.org>
2021-10-10 14:29:06 +0900

    Improve convert.sh

--- convert.sh
+++ convert.sh
@@ -2,19 +2,24 @@
 
 usage() {
     cat <<EOF
-Usage: $(basename $0)
+Usage: $(basename "$0")
 EOF
-    exit $1
+    exit "$1"
 }
 
 #size=1200x630
 size=$2
+out="$1.jpg"
 
 convert \
-    -define jpeg:size=$size \
+    -define "jpeg:size=$size" \
     -quality 92 \
-    -resize $size \
-    $1 $1
+    -resize "$size" \
+    "$1" "$out"
 # -unsharp 12x6+0.5+0 \
-exiftool -all= $1
-mv $1 $(dirname $1)/$(sha1sum $1 | cut -d ' ' -f 1).jpg
+exiftool -overwrite_original_in_place -all= "$out"
+outfilename="${3:-$(sha1sum "$1" | cut -d ' ' -f 1)}"
+outfile="$(dirname "$1")/$outfilename.jpg"
+mv "$out" "$outfile"
+echo "output to $outfile"
+mv "$1" "$1.orig"
